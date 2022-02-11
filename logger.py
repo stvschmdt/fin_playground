af40ba2f3e27954d8e966bac83b9f76b25f56480
stvschmdt <steveschmidt@uchicago.edu>
2022-02-11 10:56:00 -0500

    init account management

--- logger.py
+++ logger.py
@@ -12,25 +12,16 @@ class Logging(object):
     def __init__(self, filepath=""):
         self.filepath = filepath
         self.start = datetime.datetime.utcnow()
-        print "[log] started {}" .format(self.start)
+        print("[log] started {}" .format(self.start))
 
     def error(self, msg):
-        print "[error] {}".format(datetime.datetime.utcnow()), msg
+        print("[error] {}".format(datetime.datetime.utcnow()), msg)
 
     def info(self, msg):
-        print "[info] {}".format(datetime.datetime.utcnow()), msg
+        print("[info] {}".format(datetime.datetime.utcnow()), msg)
 
     def warning(self, msg):
-        print "[warning] {}".format(datetime.datetime.utcnow()), msg
-
-    def terminate(self, msg):
-        print "[terminated] {}".format(datetime.datetime.utcnow()), msg
-
-    def refresh(self, msg):
-        print "[refresh] {}".format(datetime.datetime.utcnow()), msg
-
-    def clockout(self):
-        print "[clock] {}".format(datetime.datetime.utcnow())
+        print("[warning] {}".format(datetime.datetime.utcnow()), msg)
 
     def results(self, msg):
-        print "[results] {}".format(datetime.datetime.utcnow()), msg
+        print("[results] {}".format(datetime.datetime.utcnow()), msg)
