c9fde9cce127d3cf6c01295f4a870ddb63d82b42
stvschmdt <steveschmidt@uchicago.edu>
2022-03-15 22:05:14 -0400

    new shell script

--- update.sh
+++ update.sh
@@ -1,8 +1,41 @@
-if [[ $1 != "setup" ]]; then
+if [[ $# -eq 0 ]] ; then
+    echo "please enter valid option {setup, push}"
+    echo "./update.sh {option}"
+    exit 0
+fi
+
+if [[ $1 == "setup" ]]; then
+        echo "archiving current data"
+        tar -cvf previous.tar storage/
+	mv previous.tar previous/
+	echo "archived files in previous/"
  	echo "polling alphavantage api" $(date -u)
 	python src/poll.py
- 	tar cvf daily.tar storage/daily/
+ 	tar cvf daily.tar storage/daily/tickers/
+ 	tar cvf features.tar storage/daily/features/
+ 	tar cvf cryptocurrencies.tar storage/daily/cryptocurrencies
+	split -b 25M daily.tar "daily.tar.part"
+	split -b 25M features.tar "features.tar.part"
+	rm daily.tar
+	rm features.tar
+	mv *.tar* gitdata
+
+elif [[ $1 == "push" ]]; then
+ 	echo "setting up tarballs for storage/daily/*"
+	echo "creating daily.tar"
+	echo "creating features.tar"
+	echo "creating cryptocurrencies.tar"
+ 	tar cvf daily.tar storage/daily/tickers/
+ 	tar cvf features.tar storage/daily/features/
+ 	tar cvf cryptocurrencies.tar storage/daily/cryptocurrencies
+	split -b 25M daily.tar "daily.tar.part"
+	split -b 25M features.tar "features.tar.part"
+	rm daily.tar
+	rm features.tar
+	mv *.tar* gitdata
+
 else
- 	echo "setting up storage/daily/ tarball"
-	tar xvf daily.tar
+    echo "please enter valid option {setup, push}"
+    echo "./update.sh {option}"
+    exit 0
 fi
