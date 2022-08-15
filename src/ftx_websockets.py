98bfd1ba0b9f990db6b1505f3ac615dcb01bcc33
cchaves123 <90115017+cchaves123@users.noreply.github.com>
2022-08-15 00:58:47 -0400

    orderbook and trades api concurrently

--- src/ftx_websockets.py
+++ src/ftx_websockets.py
@@ -19,6 +19,7 @@ import logger
 import requests
 from websocket import create_connection
 import websockets
+import websocket
 import asyncio
 
 abspath = os.path.abspath(os.getcwd())
@@ -34,30 +35,80 @@ with open(api_path) as f:
     API_secret = API_secret.strip('\n')
 
 
-msg = {}
-trades = {}
+# msg = {}
+# trades = {}
 
-async def listen():
-    url = 'wss://ftx.us/ws/'
-    global msg
-    global trades
+# async def listen():
+#     url = 'wss://ftx.us/ws/'
+#     global msg
+#     global trades
 
-    async with websockets.connect(url) as ws:
-        await ws.send(json.dumps({'op':'subscribe', 'channel':'orderbook', 'market':'BTC/USD'}))
-        await ws.send(json.dumps({'op':'subscribe', 'channel':'trades', 'market':'BTC/USD'}))
+#     async with websockets.connect(url) as ws:
+#         await ws.send(json.dumps({'op':'subscribe', 'channel':'orderbook', 'market':'BTC/USD'}))
+#         await ws.send(json.dumps({'op':'subscribe', 'channel':'trades', 'market':'BTC/USD'}))
         
-        while True:
-            #ws.send(json.dumps({'op':'subscribe', 'channel':'orderbook', 'market':'BTC/USD'}))
+#         while True:
+#             #ws.send(json.dumps({'op':'subscribe', 'channel':'orderbook', 'market':'BTC/USD'}))
 
-            msg = await ws.recv()
-            msg = json.loads(msg) 
-            print(msg)
+#             msg = await ws.recv()
+#             msg = json.loads(msg) 
+#             print(msg)
 
-            trades = await ws.recv()
-            trades = json.loads(trades)
-            print(trades)
+#             trades = await ws.recv()
+#             trades = json.loads(trades)
+#             print(trades)
 
-asyncio.get_event_loop().run_until_complete(listen())
+# asyncio.get_event_loop().run_until_complete(listen())
+
+
+#json dumps converts a python object into a json stream
+def get_ob():
+    def on_open(wsapp):
+        print("orderbook opened")
+        subscribe_message = {'op':'subscribe', 'channel':'orderbook', 'market':'BTC/USD'}
+        ws.send(json.dumps(subscribe_message))
+
+    def on_message(wsapp, message, prev=None):
+        print(f"OB Info, Received : {datetime.now()}")
+        print(message)
+
+        ###### full json payloads ######
+        # pprint.pprint(json.loads(message))
+
+    def on_close(wsapp):
+        print("closed connection")
+
+    endpoint = 'wss://ftx.us/ws/'
+    ws = websocket.WebSocketApp(endpoint,
+                                on_open=on_open,
+                                on_message=on_message,
+                                on_close=on_close)
+
+    ws.run_forever()
+
+def get_trades():
+    def on_open(wsapp):
+        print("trades opened")
+        subscribe_message = {'op':'subscribe', 'channel':'trades', 'market':'BTC/USD'}
+        ws.send(json.dumps(subscribe_message))
+
+    def on_message(wsapp, message, prev=None):
+        print(f"Trades Info, Received : {datetime.now()}")
+        print(message)
+
+        ###### full json payloads ######
+        # pprint.pprint(json.loads(message))
+
+    def on_close(wsapp):
+        print("closed connection")
+
+    endpoint = 'wss://ftx.us/ws/'
+    ws = websocket.WebSocketApp(endpoint,
+                                on_open=on_open,
+                                on_message=on_message,
+                                on_close=on_close)
+
+    ws.run_forever()
 
 # print('starting')
 # ws = create_connection('wss://ftx.us/ws/')
@@ -71,6 +122,31 @@ asyncio.get_event_loop().run_until_complete(listen())
 #     result = ws.recv()
 #     result = json.loads(result)
 #     print(result)
+
+if __name__ == '__main__':
+    #print('hi')
+    # import multiprocessing as mp
+    
+    # p1 = mp.Process(target=get_ob())
+    # p2 = mp.Process(target=get_trades())
+    # p1.start()
+    # p2.start()
+    #p1.join() # wait for completion
+    #p2.join()
+
+    
+    import threading
+    # import concurrent.futures
+
+    # with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+    #     executor.map([get_ob, get_trades], range(2))
+
+    # x = threading.Thread(target=get_ob)
+    t1 = threading.Thread(target=get_ob, daemon=True)
+    t2 = threading.Thread(target=get_trades, daemon=True)
+    t1.start()
+    t2.start()
+    input('Hit enter to terminate...\n')
 
 
 
